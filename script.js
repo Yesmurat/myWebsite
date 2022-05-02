079221a2327095bf044c9526695ca4eccfe2241d
Yesmurat <80623243+Yesmurat@users.noreply.github.com>
2022-05-02 14:23:29 +0500

    Create script.js
    It is JS file with use of Vue.js in it

---
+++ script.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    username: 'Michael',
+    me: 'Yesmurat Sagyndyk',
+    hobbieList: [
+      'Web Programming',
+      'Books(especially Stephen King)',
+      'Dombyra'
+    ],
+    userIsLoggedIn: false,
+    names: [
+      'John',
+      'Michael',
+      'Leonardo',
+      'Kakashi',
+      'Naruto',
+      'Hinata',
+      'Oops, it is special one)))'
+    ]
+  }
+});
